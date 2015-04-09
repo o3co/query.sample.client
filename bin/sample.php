5f827aec9d6370ce26582fb8373c068cace7c718
Yoshi Aoki <yoshi@44services.jp>
2015-04-10 02:10:01 +0900

    add sample client

---
+++ bin/sample.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Push response into ring Server 
+GuzzleHttp\Tests\Server::enqueue(array(file_get_contents(__DIR__ . '/../fixtures/response.txt')));
+
+$client = new GuzzleHttp\Client(['base_url' => GuzzleHttp\Tests\Server::$url]);
+
+
+$client = new O3Co\Query\Bridge\GuzzleHttp\ProxyClient($client);
+
+$qb = new O3Co\Query\Query\SimpleQueryBuilder(new O3Co\Query\Extension\CQL\CQLPersister($client));
+
+$qb
+    ->addWhere($qb->expr()->eq('name', 'foo'))
+    ->addWhere($qb->expr()->neq('age', 'hoge'))
+    ->addOrder($qb->expr()->asc('age'))
+;
+
+GuzzleHttp\Tests\Server::start();
+
+$response = $qb->getQuery()->execute();
+$data = $response->json();
+var_dump($data);
+
+GuzzleHttp\Tests\Server::stop();
